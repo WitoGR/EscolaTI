c2252867a01a28ec64ca9b52ac0614d3fc073cd1
WitoGR <rudi_becker@outlook.com>
2023-08-10 19:11:58 -0300

    init

---
+++ src/test/java/escolati7/controleparental/ControleParentalApplicationTests.java
@@ -0,0 +1,13 @@
+package escolati7.controleparental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleParentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
